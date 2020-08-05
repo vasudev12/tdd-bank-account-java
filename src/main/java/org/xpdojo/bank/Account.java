bfeabb7065848ce5459e31a6511760f11a89bd3c
vasudev12 <vasudev.v@gmail.com>
2020-08-05 08:47:51 +0530

    Add Constructor and ovveride method

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,21 @@
 package org.xpdojo.bank;
 
 public class Account {
+    public Account() {
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
